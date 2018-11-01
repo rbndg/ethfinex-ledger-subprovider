cda66edf955a71f1982aa61f7384e54d264b60b4
rbndg <rbandegi@gmail.com>
2018-11-01 13:38:56 +1100

    Create Ledger web3

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-module.exports = {
-    "hwAppEth" : require("./packages/hw-app-eth/"),
-    "web3Subprovider" : require("./packages/web3-subprovider")
+const ProviderEngine = require('web3-provider-engine')
+const FetchSubprovider = require('web3-provider-engine/subproviders/fetch')
+const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid').default
+const createLedgerSubprovider = require('@ledgerhq/web3-subprovider').default
+
+module.exports = async (efx, Web3, {networkId, rpcUrl}) => {
+  const engine = new ProviderEngine()
+
+  const getTransport = async () => {
+    return TransportNodeHid.create()
+  }
+
+  const ledger = createLedgerSubprovider(getTransport, {
+    networkId,
+    accountsLength: 5,
+    askConfirm: false,
+  })
+  engine.addProvider(ledger)
+  engine.addProvider(new FetchSubprovider({ rpcUrl }))
+  engine.start()
+  return new Web3(engine)
 }
